9be6e1d5e289dc2e38d481915e5610248588eb28
Abhijeet Brahmchari <abybrams@gmail.com>
2025-03-09 18:28:14 +0530

    Project Setup and Initialization

---
+++ src/main/java/com/ecommerce/kafkahighconcurrencyproject/KafkaHighConcurrencyProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.kafkahighconcurrencyproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaHighConcurrencyProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaHighConcurrencyProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ecommerce/kafkahighconcurrencyproject/KafkaHighConcurrencyProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.kafkahighconcurrencyproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaHighConcurrencyProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
