3509b789672a09a4b285a03464f91e71e4ae1f84
Abhijeet Brahmchari <abybrams@gmail.com>
2025-04-09 00:41:10 +0530

    Traffic Logs Service

---
+++ src/main/java/com/ecommerce/kafkahighconcurrencyproject/util/log/TrafficLogsService.java
@@ -0,0 +1,141 @@
+/**
+ * ****************************************************************************
+ *
+ * Copyright (c) 2021, FarEye and/or its affiliates. All rights
+ * reserved.
+ * ___________________________________________________________________________________
+ *
+ *
+ * NOTICE: All information contained herein is, and remains the property of
+ * FaEye and its suppliers,if any. The intellectual and technical concepts
+ * contained herein are proprietary to FarEye. and its suppliers and
+ * may be covered by us and Foreign Patents, patents in process, and are
+ * protected by trade secret or copyright law. Dissemination of this information
+ * or reproduction of this material is strictly forbidden unless prior written
+ * permission is obtained from FarEye
+ */
+
+package com.ecommerce.kafkahighconcurrencyproject.util.log;
+
+import co.fareye.config.ConfigProperty;
+import co.fareye.config.security.GatewayUser;
+import co.fareye.constant.AppConstant.ServiceCode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.log4j.Log4j2;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpMethod;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+import org.springframework.util.MultiValueMap;
+
+import java.util.UUID;
+import java.util.concurrent.Future;
+
+/**
+ * Produce the traffic logs to kafka topic
+ *
+ * @author Baldeep Singh Kwatra
+ * @since 04-Dec-2020, 12:58:19 PM
+ */
+@Service
+@Log4j2
+public class TrafficLogsService {
+
+    @Autowired
+    @Qualifier("StringKeyValueProducer")
+    private KafkaProducer<String, String> kafkaProducer;
+
+    @Autowired
+    private ConfigProperty configProperty;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+//    @Autowired
+//    Tracer tracer;
+
+    /**
+     * @param url
+     * @param requestData   Received request data
+     * @param responseData  Response data
+     * @param requestTime   Request time when request is trigger.
+     * @param responseTime  Reponse time when response has sent.
+     * @param statusCode    Status code request like 200, 400 or 500 etc..
+     * @param exception     Catch the exception if occurred.
+     * @param ipAddress     ip address where request has been made.
+     * @param headers
+     * @param queryParams
+     * @param reqComplete
+     * @param requestMethod
+     */
+    public void log(String url, String requestData, String responseData, Long requestTime, Long responseTime,
+                    int statusCode, String exception, String ipAddress, MultiValueMap<String, String> headers,
+                    MultiValueMap<String, String> queryParams, HttpMethod requestMethod, boolean outbound) {
+        String orgId = GatewayUser.getUser() == null ? "1" : GatewayUser.getUser().getOrganizationId();
+        ServiceCode serviceCode = ServiceDetails.getServiceCode();// set by dev or from url
+//        TrafficLogs trafficLogs = TrafficLogs.builder().carrierCode("")
+//                .orgId(configProperty.getOrganizationId().isEmpty() ? orgId : configProperty.getOrganizationId())
+//                .clientIp(ipAddress).url(url).headers((LinkedMultiValueMap<String, String>) headers)
+//                .exception(exception).queryString(queryParams).responseTime(responseTime).statusCode(statusCode)
+//                .responseBody(responseData).requestTime(requestTime).requestMethod(requestMethod)
+//                .requestBody(requestData).serviceCode(serviceCode != null ? serviceCode.toString() : "")
+//                .traceId(getTraceId()).outbound(outbound).build();
+//
+//        trafficLogs.setCarrierCode(ServiceDetails.getCarrierCode());
+//        insertTrafficLogs(trafficLogs);
+    }
+
+    /**
+     * Insert into traffic logs table.
+     *
+     * @param trafficLogs
+     */
+    public void insertTrafficLogs(TrafficLogs trafficLogs) {
+        try {
+            pushLogsToTopic(trafficLogs);
+        } catch (Exception e) {
+            log.error("Exception insertTrafficLogs {}", e);
+        }
+    }
+
+    @Async
+    public boolean pushLogsToTopic(TrafficLogs trafficLogs) {
+        try {
+            String value = objectMapper.writeValueAsString(trafficLogs);
+            String key = UUID.randomUUID().toString();
+            ProducerRecord<String, String> producerRecord = new ProducerRecord<>(configProperty.getTrafficLogTopic(),
+                    key, value);
+            Future<RecordMetadata> futureRecord = kafkaProducer.send(producerRecord);
+            log.info(key + " " + futureRecord.get().toString());
+            if (!futureRecord.isDone()) {
+                log.error("KafkaPushFailed {}:", futureRecord.get().toString());
+                return false;
+            } else {
+                log.info("KafkaPushSuccess");
+                return true;
+            }
+        } catch (Exception e) {
+            log.error("Exception pushLogsToTopic {}", e);
+            return false;
+        }
+    }
+
+    /**
+     * Get Trace Id of current request
+     *
+     * @return
+     */
+//    private String getTraceId() {
+//        if (tracer.activeSpan() != null) {
+//            SpanContext spanContext = tracer.activeSpan().context();
+//            if (spanContext != null) {
+//                return spanContext.toTraceId();
+//            }
+//        }
+//        return null;
+//    }
+}
